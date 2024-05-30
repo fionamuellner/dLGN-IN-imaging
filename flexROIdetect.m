262ff3a998f4eff9a92acf281b79689e743889b7
Fiona Muellner <71281970+fionamuellner@users.noreply.github.com>
2024-05-30 22:45:18 +0200

    Update and rename ROIdetect.m to flexROIdetect.m

--- flexROIdetect.m
+++ flexROIdetect.m
@@ -1,4 +1,4 @@
-function [roi,sur,orig,C,Cfilt,Cbg,M,nroi,maxnroi,copyInd,Ind,mCn,corrthr]=ROIdetect(Cn,maxdata,data,chunks,rem,bgt,maxradius,maxnroi,filters,file_name,root)
+function [roi,sur,orig,C,Cfilt,Cbg,M,nroi,maxnroi,copyInd,Ind,mCn,corrthr] = flexROIdetect(Cn,maxdata,data,chunks,rem,bgt,maxradius,maxnroi,filters,file_name,root)
 
 % ROI detection routine based on correlation-based seeding, adaptive thresholding and expansion;
 % see Muellner & Roska 2023, https://doi.org/10.1101/2023.03.22.533751 
@@ -33,8 +33,20 @@ function [roi,sur,orig,C,Cfilt,Cbg,M,nroi,maxnroi,copyInd,Ind,mCn,corrthr]=ROIde
 % corrthr: adaptive correlation threshold per ROI
 % __________________________________
 %
-% copyright: Fiona Muellner, Institute of Molecular and Clinical Ophthalmology Basel, 24.5.2024
-% license: BSD (use/copy/modify at own risk), see license.txt
+% Copyright 2024 Fiona Muellner, Institute of Molecular and Clinical Ophthalmology Basel
+% see license.txt
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+
 
 
 warning('off','images:imshow:magnificationMustBeFitForDockedFigure');
