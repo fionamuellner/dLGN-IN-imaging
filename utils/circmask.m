4d9636298651a7913f84aa0c0a774a317d82d84d
Fiona Muellner <71281970+fionamuellner@users.noreply.github.com>
2024-05-30 22:37:19 +0200

    Update circmask.m

--- utils/circmask.m
+++ utils/circmask.m
@@ -5,7 +5,19 @@ function output = circmask(r)
 %
 % __________________________________
 %
-% copyright: Fiona Muellner, Institute of Molecular and Clinical Ophthalmology Basel, 24.5.2024
+% Copyright 2024 Fiona Muellner, Institute of Molecular and Clinical Ophthalmology Basel
+% see license.txt
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
 
 s = ceil(r);
 
